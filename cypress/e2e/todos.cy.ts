c47c9d0a0cc069d44ab7d1a78061cb946521fd26
KennedyB1 <114904314+KennedyB1@users.noreply.github.com>
2023-02-09 08:18:00 +0100

    Update

---
+++ cypress/e2e/todos.cy.ts
@@ -0,0 +1,8 @@
+describe('todo application', () => {
+  it('passes', () => {
+    cy.visit('http://localhost:1234');
+  });
+  it('passes', () => {
+    cy.visit('http://localhost:1234');
+  })
+});
